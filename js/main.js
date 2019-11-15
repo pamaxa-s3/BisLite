cd3131ad6d417a6f9823e746f0cd60409b8a711e
Roman <pamaxa-s3@ukr.net>
2019-11-15 22:46:01 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-$(function(){
+$(function () {
 
 
   $('.slider__inner').slick({
     arrows: false,
     dots: true,
+  });
+
+
+  $('.works__inner').slick({
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-right.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-left.svg" alt=""></button>',
   });
 
 
